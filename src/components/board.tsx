0f727e347b342e002088dc8e01f560f3e4dfc33c
Mr-Quin <hr82003@gmail.com>
2021-05-27 16:20:22 -0700

    updated method to render board

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { makeStyles, createStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import useGameStore, { GameStore } from '../store/useGameStore'
 import MarblePiece from './marblePiece'
-import shallow from 'zustand/shallow'
 import BoardGrid from './boardGrid'
 import { Marble, Position } from '../helpers/gameUtils'
+import useDisplayStore, { DisplayStore } from '../store/useDisplayStore'
 
 const useStyles = makeStyles((theme) =>
     createStyles({
         grid: {
             userSelect: 'none',
+            touchAction: 'none',
         },
         paper: {
             padding: theme.spacing(1),
@@ -23,18 +24,25 @@ const selector = (state: GameStore) => state.pieces
 
 const Board = () => {
     const classes = useStyles()
-    const marbles = useGameStore(selector, shallow)
+    const marbles = useGameStore(selector)
+    const [gridSize, calc] = useDisplayStore(
+        useCallback((state: DisplayStore) => [state.gridSize, state.calcPos], [])
+    )
 
     return (
         <Grid className={classes.grid} container>
             <BoardGrid />
             {marbles
                 .filter((marble) => marble.pos !== Position.OFF_GRID)
-                .map((marble, i) => {
+                .map((marble) => {
+                    const { x, y } = calc(marble.pos)
                     return (
                         <MarblePiece
                             color={Marble[marble.color].toLowerCase() as any}
+                            size={gridSize * 0.8}
                             pos={marble.pos}
+                            x={x}
+                            y={y}
                             key={marble.id}
                         />
                     )
